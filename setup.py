f889f92b79620d702c5fa9f7b164d0b67c3da279
Richard Law <richard.m.law@gmail.com>
2020-08-07 16:18:08 +1200

    release v1.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='local-outlier-factor-plugin',
-    version='1.0.1',
+    version='1.0.2',
     description='pygeoapi plugin that wraps sklearn.neighbors.LocalOutlierFactor for doing outlier/novelty detection on geospatial (point) datasets',
     url='https://github.com/manaakiwhenua/local-outlier-factor-plugin',
     author='Richard Law',
